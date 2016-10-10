ea7da531ca607f2d3f5e49e5978e0e1b859a237f
George Burton <burtonageo@gmail.com>
2016-10-10 23:32:35 +0100

    Add 2 tests

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,27 @@ pub use pascal_string::{PascalString, PascalStringAppendError, PascalStringCreat
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use ascii::*;
     #[test]
-    fn it_works() {
+    fn test_string_creation() {
+        let test = "Hello, my world!".to_owned();
+        let test_pascal = PascalString::from(&test).unwrap();
+        assert_eq!(&test, test_pascal.as_str());
+    }
+
+    #[test]
+    fn test_character_append() {
+        let mut string = PascalString::new();
+        assert!(string.try_push('h').is_ok());
+        string.push('e');
+        string.push(76u8);
+        string.push('l');
+        string.push(AsciiChar::L);
+        string.push('o');
+        string.push(AsciiChar::Null);
+
+        println!("{}", string);
+        assert_eq!(string.as_str(), "heLlLo\0");
     }
 }
