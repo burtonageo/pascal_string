c01e49deb33334a77a29bcbf13ea83d36d8d1cc9
George Burton <burtonageo@gmail.com>
2016-10-15 15:45:53 +0100

    Add some documentation

--- src/utf8/pascal_string.rs
+++ src/utf8/pascal_string.rs
@@ -1,6 +1,8 @@
 use ::PASCAL_STRING_BUF_SIZE;
 
 pub struct PascalString {
+    /// The number of bytes used in the string.
     len: u8,
+    /// The internal character buffer, encoded in utf8
     chars_buf: [u8; PASCAL_STRING_BUF_SIZE]
 }
