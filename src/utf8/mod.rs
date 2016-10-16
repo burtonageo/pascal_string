9d8619294085cd11c7767dba055e58ae5b3c117b
George Burton <burtonageo@gmail.com>
2016-10-16 20:40:37 +0100

    Improve utf8 mod.rs

--- src/utf8/mod.rs
+++ src/utf8/mod.rs
@@ -1,5 +1,11 @@
+#![allow(missing_docs, unused_variables)]
+
 mod pascal_str;
 mod pascal_string;
 
 pub use self::pascal_str::{Chars, CharsMut, Lines, PascalStr};
 pub use self::pascal_string::PascalString;
+
+#[cfg(test)]
+mod tests {
+}
